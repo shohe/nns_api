1cb84e051a03a7927069fb68e46a9c33eea4c856
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-27 01:26:38 +0900

    create offer table / migration

---
+++ database/migrations/2018_08_26_162137_create_offers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('offers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cx_id');
+            $table->string('menu');
+            $table->float('price');
+            $table->dateTime('date_time');
+            $table->float('distance_range');
+            $table->geometry('from_location');
+            $table->integer('stylist_id')->nullable();
+            $table->integer('hair_type');
+            $table->string('comment')->default("");
+            $table->integer('charity_id');
+            $table->boolean('is_closed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('offers', function (Blueprint $table) {
+            //
+        });
+    }
+}
