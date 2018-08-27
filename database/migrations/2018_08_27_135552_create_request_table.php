c8ca6052bb10be9103b611778871e108064b0d83
Shohe Ohtani <canada.shohe@gmail.com>
2018-08-27 23:07:01 +0900

    create files need to take offer

---
+++ database/migrations/2018_08_27_135552_create_request_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('offer_id');
+            $table->integer('stylist_id');
+            $table->integer('price');
+            $table->string('comment')->default("");
+            $table->boolean('is_matched')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request');
+    }
+}
